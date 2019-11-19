7d827e415a8e9fb3f026a704f233b6e3342ac3fa
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-11-19 09:18:03 +0100

    2de commit met vragen incl

--- factuurbedrag/Program.cs
+++ factuurbedrag/Program.cs
@@ -18,10 +18,14 @@
  * VRAAG (het aantal dagen na factuurdatum waarop de factuur betaald is)
  * 
  * BEREKEN bedrag excl btw (het bedrag dat gegeven is door de persoon)
- * BEREKEN eventuele finan kort (bedrag excl BTW/ 100 *21)
+ * BEREKEN eventuele finan kort 
+ *              ALS     aantaldagenbetaling <= 10
+ *              DAN     financielekorting = bedragexclbtw * 2%
+ *              ANDERS  financielekorting = 0
  * BEREKEN (bedrag excl btw - finan kort)
  * BEREKEN BTW bedrag (bedrag excl btw en finan kort /100*21)
  * BEREKEN bedrag incl btw ( bedrag excl btw + bedrag btw)
+ * BEREKEN ANTWOORD
  * 
  * TOON het bedrag exclusief BTW;
  * TOON- de eventuele financiële korting (in EUR);
@@ -43,6 +47,39 @@ namespace factuurbedrag
     {
         static void Main(string[] args)
         {
+            //DECLARATIES
+            decimal  bedragexclbtw;
+            ushort aantaldagennafactgeldig, aantaldagennafact;
+            string bedragexclbtwgeldig;
+
+
+            //input
+            //VRAAG(het bedrag excl BTW)
+            Console.WriteLine("wat is uw bedrag excl BTW");
+            bedragexclbtwgeldig = Console.ReadLine();
+            // VRAAG(het aantal dagen na factuurdatum waarop de factuur betaald is)
+            Console.WriteLine("wat is het aantal dagen na factuurdatum waarop de factuur betaald is?: ");
+            aantaldagennafactgeldig = ushort.Parse(Console.ReadLine());
+            //processing
+            //BEREKEN bedrag excl btw(het bedrag dat gegeven is door de persoon)
+            //BEREKEN eventuele finan kort(bedrag excl BTW / 100 * 21)
+            //BEREKEN(bedrag excl btw - finan kort)
+            //BEREKEN BTW bedrag(bedrag excl btw en finan kort / 100 * 21)
+            //BEREKEN bedrag incl btw(bedrag excl btw + bedrag btw)
+
+            //output
+            //TOON het bedrag exclusief BTW;
+            //TOON - de eventuele financiële korting(in EUR);
+            //TOON - het bedrag exclusief BTW verminderd met de financiële korting;
+            //TOON - het BTW bedrag;
+            //TOON - het bedrag inclusief BTW
+
+
+            //wachten
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten!: ");
+            Console.ReadKey();
+
         }
     }
 }
