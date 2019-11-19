57d8c1d73f2fa351dd553c3dbf211589b1d6840c
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-11-19 08:45:06 +0100

    Add project files.

---
+++ factuurbedrag/Program.cs
@@ -0,0 +1,38 @@
+﻿//Enrick De Munter 5ITN nr3 19/11/2019
+/*OPGAVE
+ * ======
+ * Indien een klant betaalt binnen de 10 dagen na factuurdatum dan krijgt de klant 2 % financiële korting. Ontwerp een toepassing waarin de handelaar het bedrag exclusief BTW (21 %)
+ * en het aantal dagen na factuurdatum waarop de factuur betaald is, ingeeft.
+ *
+ *De toepassing toont vervolgens:
+ *
+ *- het bedrag exclusief BTW;
+ *- de eventuele financiële korting (in EUR);
+ *- het bedrag exclusief BTW verminderd met de financiële korting;
+ *- het BTW bedrag;
+ *- het bedrag inclusief BTW.
+ * 
+ * ANALYSE
+ * =======
+ * VRAAG (het bedrag excl BTW )
+ * VRAAG (het aantal dagen na factuurdatum waarop de factuur betaald is)
+ * 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factuurbedrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
